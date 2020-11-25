9a87eadcf96a8a79499a9050b63e804b7a441c95
midnightsmith89 <59675211+midnightsmith89@users.noreply.github.com>
2020-11-24 18:42:20 -0800

    name change

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -131,7 +131,7 @@
 #endif
 
 // Name displayed in the LCD "Ready" message and Info menu
-#define CUSTOM_MACHINE_NAME "Tronxy Turbo"
+#define CUSTOM_MACHINE_NAME "Tronxy Turbo 2"
 
 // Printer's unique ID, used by some programs to differentiate between machines.
 // Choose your own or use a service like https://www.uuidgenerator.net/version4
