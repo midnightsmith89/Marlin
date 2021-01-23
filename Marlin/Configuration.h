a11db8fda1ca786cf6229dc3b0b1a3de4da5e92a
midnightsmith89 <59675211+midnightsmith89@users.noreply.github.com>
2021-01-23 12:16:59 -0800

    board

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -129,7 +129,7 @@
 
 // Choose the name from boards.h that matches your setup
 #ifndef MOTHERBOARD
-  #define MOTHERBOARD BOARD_MKS_GEN_L
+  #define MOTHERBOARD BOARD_BTT_SKR_V1_4_TURBO
 #endif
 
 // Name displayed in the LCD "Ready" message and Info menu
